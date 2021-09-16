f4cf6e4e030834ea3c4c2159e38db8e07e26724d
Herb Miller <herb@bobbingwide.com>
2021-09-16 12:36:53 +0100

    Enable support for the sb-field-block plugin #24

--- oik-plugins.php
+++ oik-plugins.php
@@ -4,7 +4,7 @@ Plugin Name: oik plugins server
 Depends: oik base plugin, oik-fields
 Plugin URI: https://www.oik-plugins.com/oik-plugins/oik-plugins
 Description: oik plugins server for premium and free(mium) oik plugins
-Version: 1.20.0
+Version: 1.21.0
 Author: bobbingwide
 Author URI: https://www.bobbingwide.com/about-bobbing-wide
 Text Domain: oik-plugins
@@ -12,7 +12,7 @@ Domain Path: /languages/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-    Copyright 2012-2019 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2012-2021 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
@@ -207,7 +207,7 @@ function oik_register_oik_plugin() {
   $post_type_args = array();
   $post_type_args['label'] = 'Plugins';
   $post_type_args['description'] = 'oik plugin';
-  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone' );
+  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone', 'custom-fields' );
   $post_type_args['taxonomies'] = array( "oik_tags" );
   $post_type_args['has_archive'] = true;
   $post_type_args['menu_icon'] = 'dashicons-admin-plugins';
@@ -224,7 +224,7 @@ function oik_register_oik_plugin() {
   bw_register_field( "_oikp_slug", "text", "Plugin slug", array( '#hint' => "_oikp_slug" ) );
   bw_register_field( "_oikp_name", "text", "Plugin name", array( '#hint' => $oikp_name ) ); 
   bw_register_field( "_oikp_desc", "text", "Description" ); 
-  bw_register_field( "_oikp_git", "text", "GitHub repository", array( "#hint" => $oikp_git ) );
+  bw_register_field( "_oikp_git", "sctext", "GitHub repository", array( "#hint" => $oikp_git ) );
  bw_register_field( "_oikp_uri", "URL", "Plugin URI", array( '#hint' => 'optional', '#theme' => null ) );
  bw_register_field( "_oikp_dependency", "noderef", "Depends on", array( '#type' => 'oik-plugins', '#optional' => true, '#multiple' => 5, '#theme_null' => false ) );
 	// bw_register_field( "_oikp_banner", "noderef", "banner image link", array( '#type' => 'attachment', '#optional' => true ) );
@@ -383,7 +383,7 @@ function oik_register_oik_pluginversion() {
   
   $post_type_args['menu_icon'] = 'dashicons-shield';
   
-  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone' );
+  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone', 'custom-fields' );
   $post_type_args['show_in_rest'] = true;
   $post_type_args['template'] = oikp_oik_pluginversion_CPT_template();
   bw_register_post_type( $post_type, $post_type_args );
@@ -419,10 +419,12 @@ function oik_register_oik_pluginversion_fields( $post_type ) {
   bw_register_field( "_oikpv_version", "text", "Version", array( '#hint' => " (omit the v)" ) ); 
   bw_register_field( "_oikpv_download_count", "numeric", "Download count", array( '#theme' => false ) );
   bw_register_field( "_oikpv_update_count", "numeric", "Update count", array( '#theme' => false ) );
+  bw_register_field( "oikpv_no_underscore", "numeric", "No underscore", array( '#theme' => false ) );
   bw_register_field_for_object_type( "_oikpv_version", $post_type, true );
   bw_register_field_for_object_type( "_oikpv_plugin", $post_type, true );
   bw_register_field_for_object_type( "_oikpv_download_count", $post_type, true );
   bw_register_field_for_object_type( "_oikpv_update_count", $post_type, true );
+  bw_register_field_for_object_type( "oikpv_no_underscore", $post_type, true );
   oikp_columns_and_titles( $post_type );
 }
 
@@ -468,7 +470,7 @@ function oik_register_oik_premiumversion() {
   $post_type_args['description'] = 'oik premium plugin version';
   $post_type_args['taxonomies'] = array( "required_version", "compatible_up_to" );
   $post_type_args['menu_icon'] = 'dashicons-shield-alt';
-  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone' );
+  $post_type_args['supports'] = array( 'title', 'editor', 'thumbnail', 'excerpt', 'revisions', 'author', 'publicize', 'home', 'clone', 'custom-fields' );
 	$post_type_args['show_in_rest'] = true;
   bw_register_post_type( $post_type, $post_type_args );
   oik_register_oik_pluginversion_fields( $post_type );
@@ -790,7 +792,7 @@ function oikp_admin_menu() {
  * @param array $value - field values array
  * @param array $field - field definition
  */
-function bw_theme_field_text__oikp_git( $key, $value, $field ) {
+function bw_theme_field_sctext__oikp_git( $key, $value, $field ) {
  //bw_trace2();
  if ( $value ) {
    $github = $value[0];
